2b0639aa9de9addbbb4bcf6d2f99ae52dc53f1c4
bibing <95389491+bibing-raza@users.noreply.github.com>
2026-02-13 15:39:19 +0800

    update : penambahan no. kartu peserta bpjs pada hasil print sep

--- src/bridging/BPJSDataSEP.java
+++ src/bridging/BPJSDataSEP.java
@@ -5776,7 +5776,7 @@ public final class BPJSDataSEP extends javax.swing.JDialog {
                             + "where rp.no_rawat='" + tbSEP.getValueAt(tbSEP.getSelectedRow(), 2).toString() + "'"));
 
                     Valid.MyReport("rptBuktiRegistrasiRZvct.jasper", "report", "::[ Bukti Registrasi Rawat Jalan Poliklinik VCT ]::",
-                            "SELECT bs.no_sep, concat(p.no_rkm_medis,' (No. Reg. : ',rp.no_reg,')') no_rkm_medis, p.nm_pasien, "
+                            "SELECT bs.no_sep, bs.no_kartu, concat(p.no_rkm_medis,' (No. Reg. : ',rp.no_reg,')') no_rkm_medis, p.nm_pasien, "
                             + "concat(date_format(p.tgl_lahir,'%d/%m/%Y'),' (Usia : ',rp.umurdaftar,' ',rp.sttsumur,'.)') tgllahir, "
                             + "if(p.jk='L','Laki-laki','Perempuan') jk, date_format(rp.tgl_registrasi,'%d/%m/%Y') tglreg, "
                             + "time_format(rp.jam_reg,'%H:%i') jam, concat(pl.nm_poli,'" + antrianKhusus + "') nm_poli, d.nm_dokter, "
@@ -5808,7 +5808,7 @@ public final class BPJSDataSEP extends javax.swing.JDialog {
                                 + "and jnspelayanan='2'", param);
                     } else {
                         Valid.MyReport("rptBuktiRegistrasiRZ.jasper", "report", "::[ Bukti Registrasi Rawat Jalan ]::",
-                                "SELECT bs.no_sep, concat(p.no_rkm_medis,' (No. Reg. : ',rp.no_reg,')') no_rkm_medis, p.nm_pasien, "
+                                "SELECT bs.no_sep, bs.no_kartu, concat(p.no_rkm_medis,' (No. Reg. : ',rp.no_reg,')') no_rkm_medis, p.nm_pasien, "
                                 + "concat(date_format(p.tgl_lahir,'%d/%m/%Y'),' (Usia : ',rp.umurdaftar,' ',rp.sttsumur,'.)') tgllahir, "
                                 + "if(p.jk='L','Laki-laki','Perempuan') jk, date_format(rp.tgl_registrasi,'%d/%m/%Y') tglreg, time_format(rp.jam_reg,'%H:%i') jam, "
                                 + "concat(pl.nm_poli,'" + antrianKhusus + "') nm_poli, d.nm_dokter, ifnull(pt.nama,rp.nip_petugas) nama "
