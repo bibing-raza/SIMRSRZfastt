556223a7dd9fe7afc68ce47fc2ea3fef504d8c3d
bibing <95389491+bibing-raza@users.noreply.github.com>
2026-07-20 15:56:45 +0800

    penambahan menu baru yaitu e-RM masalak keperawatan hipovolemia (SUDAH FIX)

--- src/simrskhanza/frmUtama.java
+++ src/simrskhanza/frmUtama.java
@@ -873,7 +873,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnLogin.setForeground(new java.awt.Color(0, 0, 0));
         BtnLogin.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/lock.png"))); // NOI18N
-        BtnLogin.setMnemonic('Z');
         BtnLogin.setText("Log-in");
         BtnLogin.setToolTipText("Alt+Z");
         BtnLogin.setGlassColor(new java.awt.Color(255, 255, 255));
@@ -888,7 +887,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnCancel.setForeground(new java.awt.Color(0, 0, 0));
         BtnCancel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/exit.png"))); // NOI18N
-        BtnCancel.setMnemonic('Y');
         BtnCancel.setText("Batal");
         BtnCancel.setToolTipText("Alt+Y");
         BtnCancel.setGlassColor(new java.awt.Color(255, 255, 255));
@@ -932,7 +930,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnClosePass.setForeground(new java.awt.Color(0, 0, 0));
         BtnClosePass.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/cross.png"))); // NOI18N
-        BtnClosePass.setMnemonic('2');
         BtnClosePass.setText("Tutup");
         BtnClosePass.setToolTipText("Alt+2");
         BtnClosePass.setName("BtnClosePass"); // NOI18N
@@ -951,7 +948,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnSimpanPass.setForeground(new java.awt.Color(0, 0, 0));
         BtnSimpanPass.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/save-16x16.png"))); // NOI18N
-        BtnSimpanPass.setMnemonic('1');
         BtnSimpanPass.setText("Simpan");
         BtnSimpanPass.setToolTipText("Alt+1");
         BtnSimpanPass.setName("BtnSimpanPass"); // NOI18N
@@ -1062,7 +1058,6 @@ public class frmUtama extends javax.swing.JFrame {
         ChkInput.setBorder(null);
         ChkInput.setForeground(new java.awt.Color(0, 0, 0));
         ChkInput.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/143.png"))); // NOI18N
-        ChkInput.setMnemonic('C');
         ChkInput.setSelected(true);
         ChkInput.setText("Cari Menu");
         ChkInput.setToolTipText("Alt+C");
@@ -1087,7 +1082,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         button1.setForeground(new java.awt.Color(0, 0, 0));
         button1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/exit.png"))); // NOI18N
-        button1.setMnemonic('K');
         button1.setText("Keluar");
         button1.setToolTipText("Alt+K");
         button1.setGlassColor(new java.awt.Color(255, 255, 255));
@@ -4679,7 +4673,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnMenu.setForeground(new java.awt.Color(0, 0, 0));
         BtnMenu.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/menu.png"))); // NOI18N
-        BtnMenu.setMnemonic('M');
         BtnMenu.setText("Menu");
         BtnMenu.setToolTipText("Alt+M");
         BtnMenu.setEnabled(false);
@@ -4699,7 +4692,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         btnGantiPassword.setForeground(new java.awt.Color(0, 0, 0));
         btnGantiPassword.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/loginorg.png"))); // NOI18N
-        btnGantiPassword.setMnemonic('G');
         btnGantiPassword.setText("Ganti Password");
         btnGantiPassword.setToolTipText("Alt+G");
         btnGantiPassword.setEnabled(false);
@@ -4727,7 +4719,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnToolReg.setForeground(new java.awt.Color(0, 0, 0));
         BtnToolReg.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/PatientFile.png"))); // NOI18N
-        BtnToolReg.setMnemonic('R');
         BtnToolReg.setText("Registrasi");
         BtnToolReg.setToolTipText("Alt+R");
         BtnToolReg.setEnabled(false);
@@ -4747,7 +4738,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         btnToolIGD.setForeground(new java.awt.Color(0, 0, 0));
         btnToolIGD.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/Doctor.png"))); // NOI18N
-        btnToolIGD.setMnemonic('D');
         btnToolIGD.setText("IGD/UGD");
         btnToolIGD.setToolTipText("Alt+D");
         btnToolIGD.setEnabled(false);
@@ -4775,7 +4765,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         btnBridgingEklaim.setForeground(new java.awt.Color(0, 0, 0));
         btnBridgingEklaim.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/inacbg_eklaim_kecil.png"))); // NOI18N
-        btnBridgingEklaim.setMnemonic('E');
         btnBridgingEklaim.setText("Eklaim INACBG");
         btnBridgingEklaim.setToolTipText("Alt+E");
         btnBridgingEklaim.setEnabled(false);
@@ -4795,7 +4784,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         btnToolLab.setForeground(new java.awt.Color(0, 0, 0));
         btnToolLab.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/addressbook-edit24.png"))); // NOI18N
-        btnToolLab.setMnemonic('O');
         btnToolLab.setText("Laboratorium");
         btnToolLab.setToolTipText("Alt+O");
         btnToolLab.setEnabled(false);
@@ -4815,7 +4803,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         btnToolRad.setForeground(new java.awt.Color(0, 0, 0));
         btnToolRad.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/Surgeon.png"))); // NOI18N
-        btnToolRad.setMnemonic('A');
         btnToolRad.setText("Radiologi");
         btnToolRad.setToolTipText("Alt+A");
         btnToolRad.setEnabled(false);
@@ -4835,7 +4822,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnToolJualObat.setForeground(new java.awt.Color(0, 0, 0));
         BtnToolJualObat.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/shopping-cart-insert24.png"))); // NOI18N
-        BtnToolJualObat.setMnemonic('j');
         BtnToolJualObat.setText("Penjualan");
         BtnToolJualObat.setToolTipText("Alt+J");
         BtnToolJualObat.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -4854,7 +4840,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnDasboard.setForeground(new java.awt.Color(0, 0, 0));
         BtnDasboard.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/Pills.png"))); // NOI18N
-        BtnDasboard.setMnemonic('S');
         BtnDasboard.setText("Dashboard e-Resep");
         BtnDasboard.setToolTipText("Alt+S");
         BtnDasboard.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -4881,7 +4866,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnToolKamnap.setForeground(new java.awt.Color(0, 0, 0));
         BtnToolKamnap.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/home24.png"))); // NOI18N
-        BtnToolKamnap.setMnemonic('P');
         BtnToolKamnap.setText("Rawat Inap");
         BtnToolKamnap.setToolTipText("Alt+P");
         BtnToolKamnap.setEnabled(false);
@@ -4901,7 +4885,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnToolKasir.setForeground(new java.awt.Color(0, 0, 0));
         BtnToolKasir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/addressbook-edit24.png"))); // NOI18N
-        BtnToolKasir.setMnemonic('W');
         BtnToolKasir.setText("Rawat Jalan");
         BtnToolKasir.setToolTipText("Alt+W");
         BtnToolKasir.setEnabled(false);
@@ -4929,7 +4912,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnLog.setForeground(new java.awt.Color(0, 0, 0));
         BtnLog.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/login2.png"))); // NOI18N
-        BtnLog.setMnemonic('L');
         BtnLog.setText("Log In");
         BtnLog.setToolTipText("Alt+L");
         BtnLog.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -4948,7 +4930,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         BtnClose.setForeground(new java.awt.Color(0, 0, 0));
         BtnClose.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/Delete.png"))); // NOI18N
-        BtnClose.setMnemonic('U');
         BtnClose.setText("Keluar");
         BtnClose.setToolTipText("Alt+U");
         BtnClose.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
@@ -5074,7 +5055,7 @@ public class frmUtama extends javax.swing.JFrame {
         scrollPane1.setName("scrollPane1"); // NOI18N
 
         PanelWall.setBackground(new java.awt.Color(29, 29, 29));
-        PanelWall.setBackgroundImage(new javax.swing.ImageIcon(getClass().getResource("/picture/underconstruction.jpg"))); // NOI18N
+        PanelWall.setBackgroundImage(new javax.swing.ImageIcon(getClass().getResource("/picture/wallpaper.jpg"))); // NOI18N
         PanelWall.setBackgroundImageType(usu.widget.constan.BackgroundConstan.BACKGROUND_IMAGE_STRECT);
         PanelWall.setBorder(javax.swing.BorderFactory.createEmptyBorder(20, 0, 0, 0));
         PanelWall.setPreferredSize(new java.awt.Dimension(200, 200));
@@ -5172,7 +5153,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         jMenu1.setBackground(new java.awt.Color(20, 0, 20));
         jMenu1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/barralan.png"))); // NOI18N
-        jMenu1.setMnemonic('I');
         jMenu1.setText("Informasi");
         jMenu1.setToolTipText("Alt+I");
         jMenu1.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
@@ -5243,7 +5223,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         jMenu2.setBackground(new java.awt.Color(20, 0, 20));
         jMenu2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/search_page.png"))); // NOI18N
-        jMenu2.setMnemonic('I');
         jMenu2.setText("Tarif Penunjang Medis");
         jMenu2.setToolTipText("Alt+I");
         jMenu2.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
@@ -5290,7 +5269,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         jMenu3.setBackground(new java.awt.Color(20, 0, 20));
         jMenu3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/search_page.png"))); // NOI18N
-        jMenu3.setMnemonic('I');
         jMenu3.setText("Tarif Pelayanan Kesehatan");
         jMenu3.setToolTipText("Alt+I");
         jMenu3.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
@@ -5361,7 +5339,6 @@ public class frmUtama extends javax.swing.JFrame {
 
         jMenu4.setBackground(new java.awt.Color(20, 0, 20));
         jMenu4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/63.png"))); // NOI18N
-        jMenu4.setMnemonic('T');
         jMenu4.setText("Tentang Program");
         jMenu4.setToolTipText("Alt+T");
         jMenu4.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
